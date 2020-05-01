3ffa9bd92280894f86fc448ffaf49ff336683296
Rizwan Habeeb <rizwanhabeeb@outlook.com>
2020-05-01 20:05:45 +0530

    Sorting button styles

--- src/components/Sorting.js
+++ src/components/Sorting.js
@@ -6,26 +6,28 @@ const Sorting = (props) => {
 
     const handleClick = (type, e) => {
         e.preventDefault();
-        props.handleSort(type);
         setActive(type);
+        props.handleSort(type);
     }
+
+    console.log(active);
 
     return <Container vertical>
             <span>Sort By </span>
             <div>
                 <a 
                     href="#" 
-                    className={"sortBtn" + active == 0 ? " active":""} 
+                    className={"sortBtn" + (active == 0 ? " active":"")} 
                     onClick={(e) => {handleClick(0, e)}}>Popularity
                 </a>
                 <a 
                     href="#" 
-                    className={"sortBtn" + active == 1 ? " active":""} 
+                    className={"sortBtn" + (active == 1 ? " active":"")} 
                     onClick={(e) => {handleClick(1, e)}}>Price -- Low to High
                 </a>
                 <a 
                     href="#" 
-                    className={"sortBtn" + active == -1 ? " active":""} 
+                    className={"sortBtn" + (active == -1 ? " active":"")} 
                     onClick={(e) => {handleClick(-1, e)}}>Price -- High to Low
                 </a>
             </div>
@@ -34,13 +36,21 @@ const Sorting = (props) => {
 
 const Container = styled.div`
     background-color: #fff;
-    padding: 10px;
-    padding-top: 0;
+    padding: 0 10px;
     border-bottom: 1px solid #ddd;
+    display: flex;
+    padding-bottom: 5px;
 
     .sortBtn{
+        text-decoration: none;
+        margin: 0 10px;
+        font-size: 14px;
+        color: #212121;
+        line-height: 1.4;
+        padding-bottom: 5px;
         &.active{
-            
+            border-bottom: 2px solid #da2234;
+            color: #da2234;
         }
     }
 `
